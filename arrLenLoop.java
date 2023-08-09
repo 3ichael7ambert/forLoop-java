e4dba74cff1b9c075213a8db137d1295465f14d4
3ichael7ambert <mlambertbusiness@gmail.com>
2023-08-09 16:48:50 -0600

    add

---
+++ arrLenLoop.java
@@ -0,0 +1,11 @@
+package Java.forLoop-java;
+
+public class arrLenLoop {
+	public static void main(String[] args) {
+		int[] ages = {18, 33, 24, 64, 45};
+
+		for(int x=0;x<ages.length;x++) {
+			System.out.println(ages[x]);
+		}
+	}
+}
---
+++ arrayLoopXnums.java
@@ -0,0 +1,10 @@
+
+public class arrayLoopXnums {
+	public static void main(String[] args) {
+		int[] nums = {2, 3, 5, 7};
+
+		for (int x: nums) {
+			System.out.println(x);
+		}
+	}
+}
